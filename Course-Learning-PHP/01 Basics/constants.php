a1956f36f2f890bcd986d78b4679bb01761fc647
frederico <thecanuckatheist@gmail.com>
2021-03-19 23:34:29 -0400

    Learning PHP babygit add .

---
+++ Course-Learning-PHP/01 Basics/constants.php
@@ -0,0 +1,7 @@
+<?php
+  define('VERSION', 1.1);
+  echo VERSION;
+
+  // VERSION = 1.2; Will not work
+  // define('VERSION', 1.2); //Also will not work.
+?>
---
+++ Course-Learning-PHP/01 Basics/quotes.php
@@ -0,0 +1,14 @@
+<?php
+  $name = 'Joe';
+  echo 'Hello, ' . $name . '!';
+
+  $newName = 'Mary';
+  echo "Hello $newName !";
+
+  echo 'Welcome to Joe\'s Website.'; // backslash for escape characters
+
+  // \n for a new line    !requires "" double quotes for it to work.
+  // \\ for a backslash
+  // \$ for a dollar sign
+  // \t for a tab
+  ?>
